22c9728ce0b928f96d8570884b72fa1eb5311d01
NantipatSoftEn <keymasterviriya1150@gmail.com>
2021-03-21 08:44:58 +0700

    ⚗  Experiment new things$ 1

---
+++ write-for-inw.sql
@@ -0,0 +1,11 @@
+
+
+
+SELECT DISTINCT city FROM providers ORDER BY city ASC
+SELECT MAX(price),MIN(price) FROM products 
+SELECT CAST(AVG(price) AS DECIMAL(10,2)) FROM products
+
+-- NUMER | SQL FORMAT
+
+
+SELECT categories.name, SUM(products.amount) FROM products join categories on products.id_categories = categories.id group by  categories.name;
