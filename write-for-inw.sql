30a6e9326770e2d1321b7c9832654235eee41b54
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2021-03-21 11:17:35 +0700

    ✏️ Fix typos

--- write-for-inw.sql
+++ write-for-inw.sql
@@ -9,3 +9,17 @@ SELECT CAST(AVG(price) AS DECIMAL(10,2)) FROM products
 
 
 2609 SELECT categories.name, SUM(products.amount) FROM products join categories on products.id_categories = categories.id group by  categories.name;
+
+
+
+2614 select customers.name,rentals.rentals_date
+from customers
+inner join rentals on customers.id=rentals.id_customers
+where rentals.rentals_date>='2016-09-01' and rentals.rentals_date<='2016-09-30'
+-- you can also without inner
+2614 select customers.name,rentals.rentals_date
+from customers join rentals on customers.id=rentals.id_customers
+where rentals.rentals_date>='2016-09-01' and rentals.rentals_date<='2016-09-30'
+
+
+2615 SELECT DISTINCT customers.city from customers
